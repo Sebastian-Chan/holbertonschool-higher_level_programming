82ac3d5c679244cf20818a7541c854d285e09cff
psjrsach <sebastian.alberto.chan@gmail.com>
2026-01-29 19:16:11 -0600

    Update 9-cities_by_state_join.sql

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
 -- FILTRO DE * LAS CIUDADES Y SUS ESTADOS ASC
 SELECT cities.id, cities.name, states.name FROM cities, states
-WHERE cities.id = states.id
+WHERE cities.state_id = states.id
 ORDER BY cities.id ASC
