e2f81ece463bb6991b8093105e582e3a16ad47e2
psjrsach <sebastian.alberto.chan@gmail.com>
2026-01-29 18:18:15 -0600

    Create 6-states.sql

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- CREAR BASE DE DATOS Y LUEGO TABLA states CON id (unica, no NULL, autogenerada, es PK) y name
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states(
+	id INT NOT NULL AUTO_INCREMENT	
+	name VARCHAR(256) NOT NULL
+);
