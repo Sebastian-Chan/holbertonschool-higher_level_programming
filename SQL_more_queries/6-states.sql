a7a42b6273d1120d91e7871051a923b1f5355b01
psjrsach <sebastian.alberto.chan@gmail.com>
2026-01-29 18:22:56 -0600

    Update 6-states.sql

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,8 +1,9 @@
--- CREAR BASE DE DATOS Y LUEGO TABLA states CON id (unica, no NULL, autogenerada, es PK) y name
+-- CREAR BASE DE DATOS Y LUEGO TABLA states CON id 
+-- (unica, no NULL, autogenerada, es PK) y name
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 use hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states(
-	id INT NOT NULL AUTO_INCREMENT,	
+CREATE TABLE IF NOT EXISTS states (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,	
 	name VARCHAR(256) NOT NULL
 );
